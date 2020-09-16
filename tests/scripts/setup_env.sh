760b73153a3853ffbb2ea8a7b8c4e0af994ed95a
Joe Workman <joe.workman@jumpcloud.com>
2020-09-16 15:18:39 -0600

    build .aws directory

--- tests/scripts/setup_env.sh
+++ tests/scripts/setup_env.sh
@@ -14,14 +14,23 @@ defaults write ~/Library/Preferences/com.github.autopkg.plist JC_API "$1"
 # Set AWS S3 Creds:
 credentialsAWS=~/.aws/credentials
 configAWS=~/.aws/config
+AWS=~/.aws
+
+# Check if .aws direcotry exists:
+if [ ! -d "$AWS" ]; then
+  echo "Creating AWS Directory..."
+  mkdir $AWS
+fi
 
 # Write the credential settings
+echo "Setting AWS Credentials..."
 cat << EOF >$credentialsAWS
 aws_access_key_id = $2
 aws_secret_access_key = $3
 EOF
 
 # Write the config settings
+echo "Creating AWS Config Settings..."
 cat << EOF >$configAWS
 [default]
 region = us-east-2
@@ -29,4 +38,4 @@ EOF
 
 # Add Parent Recipe
 echo "Setting AutoPkg Repos..."
-autopkg repo-add https://github.com/autopkg/recipes > /dev/null 2>&1
+autopkg repo-add https://github.com/autopkg/recipes # > /dev/null 2>&1
